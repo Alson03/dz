7ec402204e60529ab15964eaaf219dd597c9f45d
Alson03 <svyat5955@gmail.com>
2025-11-17 22:08:34 +0700

    fix24

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func processStats(stats []string) {
 		netUsagePercent := (netUsed * 100) / netTotal
 		if netUsagePercent > 90 {
 			availableBandwidthBytes := netTotal - netUsed
-			availableBandwidthMbit := availableBandwidthBytes / (1024 * 1024 * 8)
+			availableBandwidthMbit := availableBandwidthBytes / 131072
 			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", availableBandwidthMbit)
 		}
 	}
